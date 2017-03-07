4d6569236ee0181bbf86a3c6e3c54a8f21386a31
Daniel Axtens <dja@axtens.net>
2017-03-08 10:39:04 +1100

    crc: add base aliases
    Use this for platform generic code and 32-bit code.
    
    Signed-off-by: Daniel Axtens <dja@axtens.net>

---
+++ crc/crc_base_aliases.c
@@ -0,0 +1,114 @@
+/**********************************************************************
+  Copyright(c) 2017 IBM Corporation All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions 
+  are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in
+      the documentation and/or other materials provided with the
+      distribution.
+    * Neither the name of IBM Corporation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**********************************************************************/
+
+#include "crc.h"
+#include "crc64.h"
+
+uint16_t crc16_t10dif(
+	uint16_t init_crc,        //!< initial CRC value, 16 bits
+	const unsigned char *buf, //!< buffer to calculate CRC on
+	uint64_t len              //!< buffer length in bytes (64-bit data)
+	)
+{
+	return crc16_t10dif_base(init_crc, buf, len);
+}
+
+uint32_t crc32_ieee(
+	uint32_t init_crc,        //!< initial CRC value, 32 bits
+	const unsigned char *buf, //!< buffer to calculate CRC on
+	uint64_t len              //!< buffer length in bytes (64-bit data)
+	)
+{
+	return crc32_ieee_base(init_crc, buf, len);
+}
+
+
+unsigned int crc32_iscsi(
+	unsigned char *buffer, //!< buffer to calculate CRC on
+	int len,               //!< buffer length in bytes
+	unsigned int init_crc  //!< initial CRC value
+	)
+{
+	return crc32_iscsi_base(buffer, len, init_crc);
+}
+
+uint64_t crc64_ecma_refl(
+	uint64_t init_crc,        //!< initial CRC value, 64 bits
+	const unsigned char *buf, //!< buffer to calculate CRC on
+	uint64_t len              //!< buffer length in bytes (64-bit data)
+	)
+{
+	return crc64_ecma_refl_base(init_crc, buf, len);
+}
+
+uint64_t crc64_ecma_norm(
+	uint64_t init_crc,        //!< initial CRC value, 64 bits
+	const unsigned char *buf, //!< buffer to calculate CRC on
+	uint64_t len              //!< buffer length in bytes (64-bit data)
+	)
+{
+	return crc64_ecma_norm_base(init_crc, buf, len);
+}
+
+uint64_t crc64_iso_refl(
+	uint64_t init_crc,        //!< initial CRC value, 64 bits
+	const unsigned char *buf, //!< buffer to calculate CRC on
+	uint64_t len              //!< buffer length in bytes (64-bit data)
+	)
+{
+	return crc64_iso_refl_base(init_crc, buf, len);
+}
+
+uint64_t crc64_iso_norm(
+	uint64_t init_crc,        //!< initial CRC value, 64 bits
+	const unsigned char *buf, //!< buffer to calculate CRC on
+	uint64_t len              //!< buffer length in bytes (64-bit data)
+	)
+{
+	return crc64_iso_norm_base(init_crc, buf, len);
+}
+
+uint64_t crc64_jones_refl(
+	uint64_t init_crc,        //!< initial CRC value, 64 bits
+	const unsigned char *buf, //!< buffer to calculate CRC on
+	uint64_t len              //!< buffer length in bytes (64-bit data)
+	)
+{
+	return crc64_jones_refl_base(init_crc, buf, len);
+}
+
+uint64_t crc64_jones_norm(
+	uint64_t init_crc,        //!< initial CRC value, 64 bits
+	const unsigned char *buf, //!< buffer to calculate CRC on
+	uint64_t len              //!< buffer length in bytes (64-bit data)
+	)
+{
+	return crc64_jones_norm_base(init_crc, buf, len);
+}
+
